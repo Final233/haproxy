59189747d63dbb0683bb29f729b197f32397240b
Final233 <23329853+Final233@users.noreply.github.com>
2022-08-16 15:48:31 +0800

    2022-08-16-15:48:30

--- upload.sh
+++ upload.sh
@@ -6,9 +6,6 @@
 #--------------------------------------------------------------------
 
 DATE=$(date +%F-%T)
-rm -f bak-*.tar.gz
-# git remote set-url origin https://xxxxxxx.com/cangku
-git rm -r --cached
 git add -A
 git commit -m "$DATE"
 git push && echo 上传成功
