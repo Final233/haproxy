1483b8b12f57195325d73b8bfab40d17b2611e1e
Final233 <23329853+Final233@users.noreply.github.com>
2022-08-17 10:08:50 +0800

    2022-08-17-10:08:50

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ mkdir apps
 app_name="haproxy"
 app_dir="$app_name-$version"
 app_pkg_name="$app_name-$version"
-MAKE_OPT="gmake -j $(nproc) ARCH=x86_64 TARGET=generic USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1 USE_CPU_AFFINITY=1 PREFIX=$basepath/apps/$app_dir"
+MAKE_OPT="make -j $(nproc) ARCH=x86_64 TARGET=generic USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1 USE_CPU_AFFINITY=1 PREFIX=$basepath/apps/$app_dir"
 
 _make_install() {
     wget -c http://www.haproxy.org/download/${version%.[0-9]}/src/${app_name}-${version}.tar.gz
